5bbfdf5531c301f07f01bbad2c2d272078475270
Viniciuslatini <viniciuslatini2001@gmail.com>
2023-06-24 17:21:08 -0300

    add responsive layout

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Skeleton,{ SkeletonTheme } from 'react-loading-skeleton';
+import { SkeletonTheme } from 'react-loading-skeleton';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
