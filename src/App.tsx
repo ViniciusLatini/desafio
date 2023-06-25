be4b97bd97bba5eb3eed7635dd6037d9973f6678
Viniciuslatini <viniciuslatini2001@gmail.com>
2023-06-24 21:30:08 -0300

    add API error messages

--- src/App.tsx
+++ src/App.tsx
@@ -23,35 +23,57 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [games, setGames] = useState<Game []>([]);
   const [search, setSearch] = useState('');
+  const [status, setStatus] = useState(0);
+  const [timeoutRequest, setTimeoutRequest] = useState(false);
 
   const email = "email@email.com";
   const url = "https://games-test-api-81e9fb0d564a.herokuapp.com/api/data/";
 
-  const filteredGames = search.length > 0 // Filtrando os jogos de acordo com a pesquisa
-    ? games.filter(game => game.title.includes(search))
+  const filteredGames = search.length > 0 && games.length > 0 // Filtrando os jogos de acordo com a pesquisa
+    ? games?.filter(game => game.title.includes(search))
     : [];
 
   const genres = games.length > 0 //Filtrando generos de forma unica
-    ? games.map(game => game.genre)
+    ? games?.map(game => game.genre)
     .filter((genre,i,currentValue) => currentValue.indexOf(genre) === i)
     : [];
 
   // Consulta a API retornando a lista de jogos
   useEffect(() => {
     setIsLoading(true)
-    fetch(url, {
-      method: "GET",
-      mode: "cors",
-      cache: "no-cache",
-      credentials: "same-origin",
-      headers: {
-        "Content-Type": "application/json",
-        "dev-email-address": email
-      }
-    })
-    .then(response => response.json())
-    .then(data => setGames(data))
-    .finally(() => setIsLoading(false))
+    const ac = new AbortController()
+    const timer = setTimeout(() => ac.abort(), 5000);
+
+    try {
+      fetch(url, {
+        method: "GET",
+        mode: "cors",
+        signal: ac.signal,
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers: {
+          "Content-Type": "application/json",
+          "dev-email-address": email
+        }
+      })
+      .then((response) => {
+        setStatus(response.status)
+        clearTimeout(timer);
+        return response.json()
+      })
+      .then(data => setGames(data))
+      .catch(error => {
+        console.log(error)
+      })
+      .finally(() => {
+        setIsLoading(false);
+        clearTimeout(timer);
+      })
+    } catch (error) {
+      clearTimeout(timer);
+      console.log(error);
+    }
+
   },[])
 
   return (
@@ -68,7 +90,7 @@ function App() {
         <select name="genres" >
           <option value={''} hidden> Escolha um genero</option>
           {genres.map(genre =>
-            <option id={genre} value={genre}>{genre}</option>
+            <option key={genre} id={genre} value={genre}>{genre}</option>
           )}
         </select>
 
@@ -84,41 +106,60 @@ function App() {
           <CardSkeleton/>
         </div>
       ) : (
-        <div className="cardsContainer">
-          {search.length > 0 ? (
-            filteredGames.map(game => (
-              <Card
-                title={game.title}
-                id={game.id}
-                thumbnail={game.thumbnail}
-                short_description={game.short_description}
-                game_url={game.game_url}
-                genre={game.genre}
-                platform={game.platform}
-                publisher={game.publisher}
-                developer={game.developer}
-                release_date={game.release_date}
-                freetogame_profile_url={game.freetogame_profile_url}
-              />
-            ))
+        status >= 200 && status <= 299 ? (
+          <div className="cardsContainer">
+            {search.length > 0  ? (
+              filteredGames.map(game => (
+                <Card
+                  title={game.title}
+                  id={game.id}
+                  key={game.id}
+                  thumbnail={game.thumbnail}
+                  short_description={game.short_description}
+                  game_url={game.game_url}
+                  genre={game.genre}
+                  platform={game.platform}
+                  publisher={game.publisher}
+                  developer={game.developer}
+                  release_date={game.release_date}
+                  freetogame_profile_url={game.freetogame_profile_url}
+                />
+              ))
+            ) : (
+              games.length > 0 && (
+                games.map(game => (
+                  <Card
+                    title={game.title}
+                    id={game.id}
+                    key={game.id}
+                    thumbnail={game.thumbnail}
+                    short_description={game.short_description}
+                    game_url={game.game_url}
+                    genre={game.genre}
+                    platform={game.platform}
+                    publisher={game.publisher}
+                    developer={game.developer}
+                    release_date={game.release_date}
+                    freetogame_profile_url={game.freetogame_profile_url}
+                  />
+                ))
+              )
+            )}
+          </div>
+        ) : (
+          status >= 500 && status <= 599 ? (
+            <div className="errorContainer">
+              <h1>Ops! Não conseguimos achar seus jogos ;-;</h1>
+              <p>O servidor falhou em responder, tente recarregar a página!</p>
+            </div>
           ) : (
-            games.map(game => (
-              <Card
-                title={game.title}
-                id={game.id}
-                thumbnail={game.thumbnail}
-                short_description={game.short_description}
-                game_url={game.game_url}
-                genre={game.genre}
-                platform={game.platform}
-                publisher={game.publisher}
-                developer={game.developer}
-                release_date={game.release_date}
-                freetogame_profile_url={game.freetogame_profile_url}
-              />
-            ))
-          )}
-        </div>
+            <div className="errorContainer">
+              <h1>Ops! Não conseguimos achar seus jogos ;-;</h1>
+              <p>O servidor demorou para responder, tente mais tarde</p>
+            </div>
+          )
+        )
+
       )}
     </div>
   );
