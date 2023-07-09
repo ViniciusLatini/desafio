0462e09e6bcf1e495efe19048b55243ffe685e61
Viniciuslatini <viniciuslatini2001@gmail.com>
2023-07-09 18:45:27 -0300

    create login

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,9 +1,57 @@
+import { useState, useContext } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+
+import { AuthContext } from '../../context/AuthContext';
+import './style.css'
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const history = useHistory();
+
+  const {login} = useContext(AuthContext);
+
+  function signIn() {
+    login(email, password) // .catch
+    history.push("/");
+  }
+
   return(
-    <div>
-      <input type="text"  />
-      <input type="text"  />
+    <div className="login">
+      <div className="boxLogin">
+        <h1>Login</h1>
+        <span className='subtitle'>Realize o login para manter seus games atualizados!</span>
+
+        <div className="inputContainer">
+          <span>E-mail</span>
+          <input
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+        </div>
+
+        <div className="inputContainer">
+          <span>Senha</span>
+          <input
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </div>
+
+        <button className='buttonSubmit' onClick={signIn}>
+          Confirmar Login
+        </button>
+
+        <div className="cadastroContent">
+          <span>Não possui uma conta?</span>
+          <Link to="/signup">
+            <span>Cadastre-se</span>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
