99663525aa9f4e5f6eb96f521d020298a10d1a45
Viniciuslatini <viniciuslatini2001@gmail.com>
2023-07-16 14:28:46 -0300

    add .env

--- src/service/firebase.ts
+++ src/service/firebase.ts
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBuzkpYykUxM2VJL5-I2RdX0zl2K7lzglc",
-  authDomain: "desafio-gamelist.firebaseapp.com",
-  projectId: "desafio-gamelist",
-  storageBucket: "desafio-gamelist.appspot.com",
-  messagingSenderId: "225928626233",
-  appId: "1:225928626233:web:fc8d6ceef61a75f9b24de8"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain:process.env.REACT_APP_AUTH_DOMAIN,
+  projectId:process.env.REACT_APP_PROJECT_ID,
+  storageBucket:process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId:process.env.REACT_APP_APP_ID,
 };
 
 const firebaseApp = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp()
